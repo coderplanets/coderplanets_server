1251c69edf0be933dd6019967eb3bc95ee4d7180
mydearxym <mydearxym@qq.com>
2017-11-21 11:10:30 +0800

    add mutation example

--- lib/mastani_server_web/resolvers/news_resolver.ex
+++ lib/mastani_server_web/resolvers/news_resolver.ex
@@ -5,4 +5,14 @@ defmodule MastaniServerWeb.NewsResolver do
     links = News.list_links()
     {:ok, links}
   end
+
+  def create_link(_root, args, _info) do
+    # TODO: add detailed error message handling later
+    case News.create_link(args) do
+      {:ok, link} ->
+        {:ok, link}
+      _error ->
+        {:error, "could not create link"}
+    end
+  end
 end
--- lib/mastani_server_web/schema.ex
+++ lib/mastani_server_web/schema.ex
@@ -14,4 +14,13 @@ defmodule MastaniServerWeb.Schema do
       resolve &NewsResolver.all_links/3
     end
   end
+
+  mutation do
+    field :create_link, :link do
+      arg :url, non_null(:string)
+      arg :description, non_null(:string)
+
+      resolve &NewsResolver.create_link/3
+    end
+  end
 end
