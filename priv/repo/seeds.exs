fc13b5fb3b33653b105fc9fa459ad017914d437c
mydearxym <mydearxym@qq.com>
2017-12-19 16:27:04 +0800

    wip

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,14 +1,5 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     MastaniServer.Repo.insert!(%MastaniServer.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+
+# use /test/support/populater
 
 alias MastaniServer.Populater
 
--- test/support/populater.ex
+++ test/support/populater.ex
@@ -1,3 +1,10 @@
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     MastaniServer.Repo.insert!(%MastaniServer.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
 
 alias MastaniServer.Accounts.User
 alias MastaniServer.Repo
