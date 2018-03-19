ab32cd2b5d17f86b160deb07328100c627b75146
mydearxym <mydearxym@qq.com>
2018-03-19 15:26:34 +0800

    docker setup

---
+++ docker/package_builder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PRJ_DIR="./"
+BRANCH_NAME="master"
+ARCHIVE_NAME="mastani_server.tar.gz"
+
+cd "${PRJ_DIR}"
+git checkout "${BRANCH_NAME}"
+git fetch --all
+git reset --hard origin/"${BRANCH_NAME}"
+
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+
+tar czf "${ARCHIVE_NAME}" _build/ config/ deps/ lib/ mix.exs  mix.lock  priv/
