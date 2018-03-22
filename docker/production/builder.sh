0d64280c7d83cce0700ff92011d1364ccefd8dd6
mydearxym <mydearxym@qq.com>
2018-03-22 19:45:10 +0800

    publish wip

--- docker/production/builder.sh
+++ docker/production/builder.sh
@@ -21,4 +21,5 @@ echo "[Step 3/${TOTAL_STEPS}] creating ${ARCHIVE_NAME} ..."
 tar czf "${ARCHIVE_NAME}" _build/ config/ deps/ lib/ mix.exs  mix.lock  priv/ test/
 
 echo "[Step 4/${TOTAL_STEPS}] ${ARCHIVE_NAME} created!"
+echo "------------------------------------------------"
 echo "[Step 4/${TOTAL_STEPS}] run git push to push the code to ali-cloud to finish!"
