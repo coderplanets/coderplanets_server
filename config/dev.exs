faa475dd6728dd17015e9a993b84898003ac1231
mydearxym <mydearxym@qq.com>
2018-03-23 19:16:18 +0800

    travis fix

--- config/dev.exs
+++ config/dev.exs
@@ -36,6 +36,6 @@ config :logger, :console, format: "[$level] $message\n"
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# if File.exists?("./dev.secret.exs") do
-import_config "dev.secret.exs"
-# end
+if File.exists?("./dev.secret.exs") do
+  import_config "dev.secret.exs"
+end
