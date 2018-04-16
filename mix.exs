1df61d1c7c4a9fa06932ba9722dc2aa20a31a850
mydearxym <mydearxym@qq.com>
2018-04-16 20:36:32 +0800

    update dataloader

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule MastaniServer.Mixfile do
       {:scrivener_ecto, "~> 1.3.0"},
       {:guardian, "~> 1.0"},
       {:timex, "~> 3.2.1"},
-      {:dataloader, "~> 1.0.0"},
+      {:dataloader, "~> 1.0.2"},
       {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
       {:ex_unit_notifier, "~> 0.1", only: :test},
       {:apollo_tracing, "~> 0.4.1"},
