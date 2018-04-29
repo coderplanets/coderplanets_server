e1840d0f46645e6bde7ca301a35e88e9d354dafc
mydearxym <mydearxym@qq.com>
2018-04-30 00:18:40 +0800

    seeds for comments

---
+++ priv/mock/post_comments_seeds.exs
@@ -0,0 +1,19 @@
+import MastaniServer.Factory
+
+alias MastaniServer.{CMS, Accounts}
+
+{:ok, user} = db_insert(:user)
+{:ok, post} = db_insert(:post)
+
+Enum.reduce(1..5, [], fn _, acc ->
+  {:ok, value} =
+    CMS.create_comment(
+      :post,
+      :comment,
+      post.id,
+      %Accounts.User{id: user.id},
+      Faker.Lorem.Shakespeare.king_richard_iii()
+    )
+
+  acc ++ [value]
+end)
