e677f1254798f18f7fc0fd3b4ca03c52045acc66
mydearxym <mydearxym@qq.com>
2018-06-01 20:19:54 +0800

    fmt

--- lib/helper/nested_filter.ex
+++ lib/helper/nested_filter.ex
@@ -6,7 +6,7 @@ defmodule Helper.NestedFilter do
   @type key :: any
   @type val :: any
   @type keys_to_select :: list
-  @type predicate :: ((key, val) -> boolean)
+  @type predicate :: (key, val -> boolean)
 
   @spec drop_by(struct, predicate) :: struct
   def drop_by(%_{} = struct, _), do: struct
@@ -14,9 +14,9 @@ defmodule Helper.NestedFilter do
   @spec drop_by(map, predicate) :: map
   def drop_by(map, predicate) when is_map(map) do
     map
-    |> Enum.reduce(%{},
-    fn ({key, val}, acc) ->
+    |> Enum.reduce(%{}, fn {key, val}, acc ->
       cleaned_val = drop_by(val, predicate)
+
       if predicate.(key, cleaned_val) do
         acc
       else
@@ -40,7 +40,7 @@ defmodule Helper.NestedFilter do
   """
   @spec drop_by_value(%{any => any}, [any]) :: %{any => any}
   def drop_by_value(map, values_to_reject) when is_map(map) do
-    drop_by(map, fn (_, val) -> val in values_to_reject end)
+    drop_by(map, fn _, val -> val in values_to_reject end)
   end
 
   @doc """
@@ -49,18 +49,16 @@ defmodule Helper.NestedFilter do
   """
   @spec drop_by_key(%{any => any}, [any]) :: %{any => any}
   def drop_by_key(map, keys_to_reject) when is_map(map) do
-    drop_by(map, fn(key, _) -> key in keys_to_reject end)
+    drop_by(map, fn key, _ -> key in keys_to_reject end)
   end
 
   @spec take_by(map, keys_to_select) :: map
   def take_by(map, keys_to_select) when is_map(map) do
     map
-    |> Enum.reduce(%{},
-    fn ({key, val}, acc) ->
+    |> Enum.reduce(%{}, fn {key, val}, acc ->
       Map.merge(acc, take_by(val, keys_to_select))
     end)
-    |>
-    Map.merge(Map.take(map, keys_to_select))
+    |> Map.merge(Map.take(map, keys_to_select))
   end
 
   def take_by(_elem, _) do
@@ -73,8 +71,6 @@ defmodule Helper.NestedFilter do
   """
   @spec take_by_key(%{any => any}, [any]) :: %{any => any}
   def take_by_key(map, keys_to_select) when is_map(map) do
-    Map.merge(
-      take_by(map, keys_to_select),
-      Map.take(map, keys_to_select))
+    Map.merge(take_by(map, keys_to_select), Map.take(map, keys_to_select))
   end
 end
