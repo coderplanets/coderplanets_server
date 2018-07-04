cb49ca84715751522a50bb6fa9300ed831638e3e
mydearxym <mydearxym@qq.com>
2018-07-04 16:40:19 +0800

    add notifications table / schema

---
+++ lib/mastani_server/delivery/notification.ex
@@ -0,0 +1,35 @@
+defmodule MastaniServer.Delivery.Notification do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias MastaniServer.Accounts.User
+  alias MastaniServer.Delivery.Notification
+
+  @required_fields ~w(from_user_id to_user_id action source_title source_id source_type source_preview)a
+  @optional_fields ~w(parent_id parent_type read)a
+
+  schema "notifications" do
+    belongs_to(:from_user, User)
+    belongs_to(:to_user, User)
+    field(:action, :string)
+
+    field(:source_id, :string)
+    field(:source_preview, :string)
+    field(:source_title, :string)
+    field(:source_type, :string)
+    field(:parent_id, :string)
+    field(:parent_type, :string)
+    field(:read, :boolean)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Notification{} = notification, attrs) do
+    notification
+    |> cast(attrs, @optional_fields ++ @required_fields)
+    |> validate_required(@required_fields)
+    |> foreign_key_constraint(:from_user_id)
+    |> foreign_key_constraint(:to_user_id)
+  end
+end
---
+++ priv/repo/migrations/20180704083236_create_delivery_notifications.exs
@@ -0,0 +1,24 @@
+defmodule MastaniServer.Repo.Migrations.CreateDeliveryNotifications do
+  use Ecto.Migration
+
+  def change do
+    create table(:notifications) do
+      add(:from_user_id, references(:users, on_delete: :nothing), null: false)
+      add(:to_user_id, references(:users, on_delete: :nothing), null: false)
+      add(:action, :string)
+
+      add(:source_type, :string)
+      add(:source_id, :string)
+      add(:source_title, :string)
+      add(:source_preview, :string)
+      add(:parent_type, :string)
+      add(:parent_id, :string)
+
+      add(:read, :boolean, default: false)
+      timestamps()
+    end
+
+    create(index(:notifications, [:from_user_id]))
+    create(index(:notifications, [:to_user_id]))
+  end
+end
