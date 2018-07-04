2ff78afe452f8f3469b8394bc53a6de4367e089d
mydearxym <mydearxym@qq.com>
2018-07-04 16:58:22 +0800

    add notication mail table / schema

---
+++ lib/mastani_server/accounts/notification.mail.ex
@@ -0,0 +1,33 @@
+defmodule MastaniServer.Accounts.NotificationMail do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias MastaniServer.Accounts.{User, NotificationMail}
+
+  @required_fields ~w(from_user_id to_user_id action source_id source_type source_preview)a
+  @optional_fields ~w(parent_id parent_type read)a
+
+  schema "notification_mails" do
+    belongs_to(:from_user, User)
+    belongs_to(:to_user, User)
+    field(:action, :string)
+
+    field(:source_id, :string)
+    field(:source_preview, :string)
+    field(:source_title, :string)
+    field(:source_type, :string)
+    field(:parent_id, :string)
+    field(:parent_type, :string)
+    field(:read, :boolean)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%NotificationMail{} = notication_mail, attrs) do
+    notication_mail
+    |> cast(attrs, @optional_fields ++ @required_fields)
+    |> validate_required(@required_fields)
+    |> foreign_key_constraint(:from_user_id)
+    |> foreign_key_constraint(:to_user_id)
+  end
+end
---
+++ priv/repo/migrations/20180704084815_create_account_notification_mails.exs
@@ -0,0 +1,24 @@
+defmodule MastaniServer.Repo.Migrations.CreateAccountNotificationMails do
+  use Ecto.Migration
+
+  def change do
+    create table(:notification_mails) do
+      add(:action, :string)
+      add(:source_type, :string)
+      add(:source_id, :string)
+      add(:source_title, :string)
+      add(:source_preview, :string)
+      add(:parent_type, :string)
+      add(:parent_id, :string)
+      add(:from_user_id, references(:users, on_delete: :nothing), null: false)
+      add(:to_user_id, references(:users, on_delete: :nothing), null: false)
+
+      add(:read, :boolean, default: false)
+
+      timestamps()
+    end
+
+    create(index(:notification_mails, [:from_user_id]))
+    create(index(:notification_mails, [:to_user_id]))
+  end
+end
