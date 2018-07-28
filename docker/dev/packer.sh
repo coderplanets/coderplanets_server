2121ccd5203f7fed3a7a714b15bc54eb4099bbc3
mydearxym <mydearxym@qq.com>
2018-07-28 12:36:56 +0800

    build refactor

---
+++ docker/dev/packer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ENV="dev"
+
+PRJ_DIR="./"
+BRANCH_NAME="dev"
+ARCHIVE_NAME="./docker/${ENV}/api_server.tar.gz"
+TOTAL_STEPS=5
+
+cd "${PRJ_DIR}"
+
+# echo "[Step 1/${TOTAL_STEPS}] mix deps.get ..."
+# mix deps.get
+# echo "[Step 2/${TOTAL_STEPS}] MIX_ENV=${ENV} mix compile ..."
+# MIX_ENV="${ENV}" mix compile
+
+echo "[Step 3/${TOTAL_STEPS}] creating ${ARCHIVE_NAME} ..."
+# tar czvf "${ARCHIVE_NAME}" _build/ config/ deps/ lib/ mix.exs  mix.lock  priv/ test/
+tar czvf "${ARCHIVE_NAME}" config/ lib/ mix.exs  mix.lock  priv/
+
+echo "[Step 4/${TOTAL_STEPS}] ${ARCHIVE_NAME} created!"
+echo "------------------------------------------------"
+echo "[Step 4/${TOTAL_STEPS}] run git push to push the code to ali-cloud to finish!"
--- publish.sh
+++ publish.sh
@@ -3,12 +3,12 @@
 ENV="$1"
 
 if [ "$ENV" == "prod" ];then
-    echo "[Step 0/5] running ./docker/production/builder.sh"
-    ./docker/production/builder.sh
+    echo "[Step 0/5] running ./docker/production/packer.sh"
+    ./docker/production/packer.sh
 elif [ "$ENV" == "dev" ]
 then
-    echo "[Step 0/5] running ./docker/dev/builder.sh"
-    ./docker/dev/builder.sh
+    echo "[Step 0/5] running ./docker/dev/packer.sh"
+    ./docker/dev/packer.sh
 else
     echo "invalid publish env, support env: dev / prod"
     echo "usage: ./publish.sh dev OR ./publish.sh prod"
