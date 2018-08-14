bbbe63d6dbf06c88728dfe34e12c41a38d368fd4
mydearxym <mydearxym@qq.com>
2018-08-14 11:09:01 +0800

    build: improve build

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mastani_server, MastaniServerWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80]
+  url: [host: "coderplanets.com", port: 80]
 
 # cache_static_manifest: "priv/static/cache_manifest.json"
 
