6d9a7a9abc45e38bf92c2144f05e669d9247c880
mydearxym <mydearxym@qq.com>
2018-09-28 23:12:22 +0800

    refactor(cms.repo): use new repo fields

---
+++ priv/repo/migrations/20180928141144_rename_repos_fiels.exs
@@ -0,0 +1,12 @@
+defmodule MastaniServer.Repo.Migrations.RenameReposFiels do
+  use Ecto.Migration
+
+  def change do
+    rename(table(:cms_repos), :issuesCount, to: :issues_count)
+    rename(table(:cms_repos), :prsCount, to: :prs_count)
+    rename(table(:cms_repos), :forkCount, to: :fork_count)
+    rename(table(:cms_repos), :watchCount, to: :watch_count)
+
+    rename(table(:cms_repos), :releaseTag, to: :release_tag)
+  end
+end
--- test/mastani_server/cms/repo_test.exs
+++ test/mastani_server/cms/repo_test.exs
@@ -21,7 +21,7 @@ defmodule MastaniServer.Test.Repo do
       assert {:error, _} = ORM.find_by(Author, user_id: user.id)
 
       {:ok, repo} = CMS.create_content(community, :repo, repo_attrs, user)
-      assert repo.repo_name == repo_attrs.repo_name
+      assert repo.title == repo_attrs.title
     end
 
     test "add user to cms authors, if the user is not exsit in cms authors",
