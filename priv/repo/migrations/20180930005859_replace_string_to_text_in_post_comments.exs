35cf03b37d0d8ef2cc586fdc095cd8f240f33575
mydearxym <mydearxym@qq.com>
2018-09-30 09:05:55 +0800

    refactor(comments): replace body:string -> body:text

---
+++ priv/repo/migrations/20180930005859_replace_string_to_text_in_post_comments.exs
@@ -0,0 +1,10 @@
+defmodule MastaniServer.Repo.Migrations.ReplaceStringToTextInPostComments do
+  use Ecto.Migration
+
+  def change do
+    alter table(:posts_comments) do
+      remove(:body)
+      add(:body, :text)
+    end
+  end
+end
