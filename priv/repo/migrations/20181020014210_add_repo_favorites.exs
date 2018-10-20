10ce7d0be93c58ab0dae430f11a32a18a362a2d4
mydearxym <mydearxym@qq.com>
2018-10-20 10:03:48 +0800

    feat(reactions): repo favorite with basic test

---
+++ priv/repo/migrations/20181020014210_add_repo_favorites.exs
@@ -0,0 +1,16 @@
+defmodule MastaniServer.Repo.Migrations.AddRepoFavorites do
+  use Ecto.Migration
+
+  def change do
+    create table(:repos_favorites) do
+      add(:user_id, references(:users, on_delete: :delete_all), null: false)
+      add(:repo_id, references(:cms_repos, on_delete: :delete_all), null: false)
+      add(:category_id, references(:favorite_categories, on_delete: :delete_all))
+
+      timestamps()
+    end
+
+    create(index(:repos_favorites, [:category_id]))
+    create(unique_index(:repos_favorites, [:user_id, :repo_id]))
+  end
+end
---
+++ test/mastani_server/cms/repo_reactions_test.exs
@@ -0,0 +1,33 @@
+defmodule MastaniServer.Test.RepoReactions do
+  use MastaniServer.TestTools
+
+  alias MastaniServer.CMS
+
+  setup do
+    {:ok, user} = db_insert(:user)
+    {:ok, community} = db_insert(:community)
+
+    repo_attrs = mock_attrs(:repo, %{community_id: community.id})
+
+    {:ok, ~m(user community repo_attrs)a}
+  end
+
+  describe "[cms repo star/favorite reaction]" do
+    @tag :wip
+    test "favorite and undo favorite reaction to repo", ~m(user community repo_attrs)a do
+      {:ok, repo} = CMS.create_content(community, :repo, repo_attrs, user)
+
+      {:ok, _} = CMS.reaction(:repo, :favorite, repo.id, user)
+      {:ok, reaction_users} = CMS.reaction_users(:repo, :favorite, repo.id, %{page: 1, size: 1})
+      reaction_users = reaction_users |> Map.get(:entries)
+      assert 1 == reaction_users |> Enum.filter(fn ruser -> user.id == ruser.id end) |> length
+
+      # undo test
+      {:ok, _} = CMS.undo_reaction(:repo, :favorite, repo.id, user)
+      {:ok, reaction_users2} = CMS.reaction_users(:repo, :favorite, repo.id, %{page: 1, size: 1})
+      reaction_users2 = reaction_users2 |> Map.get(:entries)
+
+      assert 0 == reaction_users2 |> Enum.filter(fn ruser -> user.id == ruser.id end) |> length
+    end
+  end
+end
