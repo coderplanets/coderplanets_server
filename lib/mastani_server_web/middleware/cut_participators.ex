b4183620c1b0d3bfc66754eda26aab9145f32b8e
mydearxym <mydearxym@qq.com>
2018-10-20 15:42:51 +0800

    chore(docs): comments dataloader

--- lib/mastani_server_web/middleware/cut_participators.ex
+++ lib/mastani_server_web/middleware/cut_participators.ex
@@ -1,21 +1,23 @@
-# ---
-# cut comments participators manually
-#
-# ---
 defmodule MastaniServerWeb.Middleware.CutParticipators do
+  @moduledoc """
+  # cut comments participators manually by count
+  # this tem solution may have performace issue when the content's comments
+  # has too much participators
+  #
+  # NOTE: this is NOT the right solution
+  # should use WINDOW function
+  # see https://github.com/coderplanets/coderplanets_server/issues/16
+  #
+  """
+
   @behaviour Absinthe.Middleware
-  # google: must appear in the GROUP BY clause or be used in an aggregate function
+  @count 5
 
   def call(%{errors: errors} = resolution, _) when length(errors) > 0, do: resolution
 
   def call(%{value: value} = resolution, _) do
-    # IO.inspect value |> Enum.slice(0, 5), label: "hello value --> "
-    %{resolution | value: value |> Enum.slice(0, 5)}
+    %{resolution | value: value |> Enum.slice(0, @count)}
   end
-
-  # def call(%{value: []} = resolution, _) do
-  #   %{resolution | value: 0}
-  # end
 
   def call(resolution, _), do: resolution
 end
