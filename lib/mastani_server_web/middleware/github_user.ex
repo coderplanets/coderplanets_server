b922b49e2021a1288863a8f9bad6974e979e163c
mydearxym <mydearxym@qq.com>
2018-11-07 12:49:35 +0800

    build: production

--- lib/mastani_server_web/middleware/github_user.ex
+++ lib/mastani_server_web/middleware/github_user.ex
@@ -9,12 +9,12 @@ defmodule MastaniServerWeb.Middleware.GithubUser do
 
     case Github.user_profile(code) do
       {:ok, user} ->
-        IO.inspect user,label: "user_profile"
+        IO.inspect(user, label: "user_profile")
         arguments = resolution.arguments |> Map.merge(%{github_user: user})
         %{resolution | arguments: arguments}
 
       {:error, err_msg} ->
-        IO.inspect err_msg, label: "user_profile error"
+        IO.inspect(err_msg, label: "user_profile error")
 
         resolution
         |> handle_absinthe_error(err_msg)
