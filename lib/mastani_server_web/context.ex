499fb86f580f2f5294ae4c7abf216ab0b5f63e92
mydearxym <mydearxym@qq.com>
2018-11-07 23:21:57 +0800

    build: production

--- lib/mastani_server_web/context.ex
+++ lib/mastani_server_web/context.ex
@@ -31,7 +31,7 @@ defmodule MastaniServerWeb.Context do
          {:ok, cur_user} <- authorize(token) do
       IO.inspect(conn.req_headers, label: "hello conn req_headers")
       IO.inspect(conn, label: "hello conn")
-      # IO.inspect(get_req_header(conn, "X-Forwarded-For"), label: "the fucking X-Forwarded-For")
+      IO.inspect(get_req_header(conn, "X-Forwarded-For"), label: "the fucking x-forwarded-for")
       IO.inspect(conn.remote_ip, label: "build content -##-> conn.remote_ip")
       %{cur_user: cur_user, remote_ip: conn.remote_ip}
     else
