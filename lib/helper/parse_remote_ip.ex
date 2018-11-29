6d7843a91d25cf9c2d0ad164797a26a9535c24fc
mydearxym <mydearxym@qq.com>
2018-11-29 11:57:30 +0800

    build: production

--- lib/helper/parse_remote_ip.ex
+++ lib/helper/parse_remote_ip.ex
@@ -6,11 +6,20 @@ defmodule Helper.RemoteIP do
   # remote ip is the fisrt ip in the proxy_ips chain
   def parse([proxy_ips, _]) do
     client_ip = proxy_ips |> String.split(",") |> List.first()
+    case client_ip not in slb_ips do
+      true ->
+        {:ok, client_ip}
 
-    {:ok, client_ip}
+      false ->
+        {:error, "skip"}
+    end
   end
 
   def parse(_) do
     {:error, "NOT_FOUND"}
+  end
+
+  defp slb_ips do
+    ["47.100.162.182"]
   end
 end
