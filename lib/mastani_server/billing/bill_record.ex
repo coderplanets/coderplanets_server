1dca350072b1db89c43d66b95da7605519db9c28
mydearxym <mydearxym@qq.com>
2018-12-06 17:03:11 +0800

    refactor(billing): add note for additonal_info

--- lib/mastani_server/billing/bill_record.ex
+++ lib/mastani_server/billing/bill_record.ex
@@ -10,7 +10,7 @@ defmodule MastaniServer.Billing.BillRecord do
   alias MastaniServer.Accounts
 
   @required_fields ~w(user_id hash_id state amount payment_usage payment_method)a
-  # @optional_fields ~w(label geo_info index)a
+  @optional_fields ~w(note)a
 
   schema "bill_records" do
     belongs_to(:user, Accounts.User, foreign_key: :user_id)
@@ -22,13 +22,15 @@ defmodule MastaniServer.Billing.BillRecord do
     field(:payment_usage, :string)
     field(:payment_method, :string)
 
+    field(:note, :string)
+
     timestamps(type: :utc_datetime)
   end
 
   def changeset(%BillRecord{} = bill_record, attrs) do
     bill_record
     # |> cast(attrs, @optional_fields ++ @required_fields)
-    |> cast(attrs, @required_fields)
+    |> cast(attrs, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
     # |> validate_length(:title, min: 1, max: 30)
     |> foreign_key_constraint(:user_id)
