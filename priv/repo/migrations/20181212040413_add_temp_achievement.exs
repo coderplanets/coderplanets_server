d852b85184d7de97a4da1f3a554d36d4e462686e
mydearxym <mydearxym@qq.com>
2018-12-12 12:19:15 +0800

    ci: debug migration

--- priv/repo/migrations/20181212040413_add_temp_achievement.exs
+++
@@ -1,9 +0,0 @@
-defmodule MastaniServer.Repo.Migrations.AddTempAchievement do
-  use Ecto.Migration
-
-  def change do
-    alter table(:user_achievements) do
-      add(:seninor_member, :boolean, default: false)
-    end
-  end
-end
