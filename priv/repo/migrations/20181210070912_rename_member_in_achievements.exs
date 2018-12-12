7b8a7a49404f88b3711c7dad108f442ba367a528
mydearxym <mydearxym@qq.com>
2018-12-12 12:26:58 +0800

    ci: debug migration again

--- priv/repo/migrations/20181210070912_rename_member_in_achievements.exs
+++ priv/repo/migrations/20181210070912_rename_member_in_achievements.exs
@@ -4,7 +4,7 @@ defmodule MastaniServer.Repo.Migrations.RenameMemberInAchievements do
   def change do
     # rename(table(:user_achievements), :seninor_member, to: :senior_member)
     alter table(:user_achievements) do
-      add(:senior_member, :boolean, default: false)
+      # add(:senior_member, :boolean, default: false)
     end
   end
 end
