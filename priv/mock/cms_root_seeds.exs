c772a4d93524d34bc2e09117fc0a2c95ea2fa03e
mydearxym <mydearxym@qq.com>
2018-12-27 22:40:13 +0800

    refactor(seeds): add cms root seeds & better func name

---
+++ priv/mock/cms_root_seeds.exs
@@ -0,0 +1,19 @@
+import MastaniServer.Support.Factory
+import Helper.Utils, only: [map_key_stringify: 1]
+
+alias MastaniServer.{Accounts, CMS}
+
+root_rules = %{
+  "root" => true
+}
+
+github_profile = mock_attrs(:github_profile, %{login: "cms_root"}) |> map_key_stringify
+
+{:ok, %{token: token, user: user}} = Accounts.github_signin(github_profile)
+{:ok, _passport} = CMS.stamp_passport(root_rules, user)
+
+IO.puts("========== create cms root done ! ===========================")
+IO.puts("paste token string to brower localstorage (fmt: token: token)")
+IO.puts("-------------------------------------------------------------")
+IO.puts(token)
+IO.puts("=============================================================")
