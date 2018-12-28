287b74eb3383ec1033176251dc1b3c54600aa479
mydearxym <mydearxym@qq.com>
2018-12-28 10:31:42 +0800

    refactor(seeds): change communities seed order, pl first

--- priv/mock/cps_seeds.exs
+++ priv/mock/cps_seeds.exs
@@ -1,15 +1,15 @@
 alias MastaniServer.CMS
 
 # NOTE: seed order matters
-CMS.seed_communities(:pl)
-CMS.seed_communities(:framework)
+CMS.seed_communities(:home)
+CMS.seed_communities(:city)
 CMS.seed_communities(:editor)
 CMS.seed_communities(:design)
-CMS.seed_communities(:database)
-CMS.seed_communities(:devops)
 CMS.seed_communities(:blockchain)
-CMS.seed_communities(:city)
-CMS.seed_communities(:home)
+CMS.seed_communities(:devops)
+CMS.seed_communities(:database)
+CMS.seed_communities(:framework)
+CMS.seed_communities(:pl)
 
 CMS.seed_set_category(["css", "antd"], "design")
 
