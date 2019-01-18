97d8dae698ffa0a7e737fcb7ac56463b1e9bb085
mydearxym <mydearxym@qq.com>
2019-01-18 20:17:50 +0800

    refactor(endpoint): try redirect www -> non-www

--- mix.exs
+++ mix.exs
@@ -93,7 +93,9 @@ defmodule MastaniServer.Mixfile do
       {:excoveralls, "~> 0.8", only: :test},
       {:sentry, "~> 6.4"},
       {:recase, "~> 0.3.0"},
-      {:nanoid, "~> 2.0.0"}
+      {:nanoid, "~> 2.0.0"},
+      # redirect www -> non-www
+      {:plug_canonical_host, "~> 0.3.1"}
     ]
   end
 
