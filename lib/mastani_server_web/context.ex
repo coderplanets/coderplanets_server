01de3d8d5ba811beda428daeee6e53c41757bd2c
mydearxym <mydearxym@qq.com>
2019-01-27 17:16:24 +0800

    build: production

--- lib/mastani_server_web/context.ex
+++ lib/mastani_server_web/context.ex
@@ -28,9 +28,12 @@ defmodule MastaniServerWeb.Context do
   def build_context(conn) do
     with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
          {:ok, cur_user} <- authorize(token) do
+      IO.inspect(conn, label: "hello conn")
 
-      IO.inspect conn, label: "hello conn"
-      IO.inspect RemoteIP.parse(get_req_header(conn, "x-forwarded-for")), label: "x-forwarded-for"
+      IO.inspect(
+        RemoteIP.parse(get_req_header(conn, "x-forwarded-for")),
+        label: "x-forwarded-for"
+      )
 
       case RemoteIP.parse(get_req_header(conn, "x-forwarded-for")) do
         {:ok, remote_ip} ->
