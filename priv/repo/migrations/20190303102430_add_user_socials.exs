e228086c211aa65e20fdce9e5b45edb7fff9ebf2
mydearxym <mydearxym@qq.com>
2019-03-03 18:52:35 +0800

    refactor(account): add user_socials table

---
+++ priv/repo/migrations/20190303102430_add_user_socials.exs
@@ -0,0 +1,26 @@
+defmodule MastaniServer.Repo.Migrations.AddUserSocials do
+  use Ecto.Migration
+
+  def change do
+    create table(:user_socials) do
+      add(:user_id, references(:users, on_delete: :delete_all), null: false)
+
+      add(:github, :string)
+      add(:twitter, :string)
+      add(:facebook, :string)
+      add(:zhihu, :string)
+      add(:dribble, :string)
+      add(:huaban, :string)
+      add(:douban, :string)
+
+      add(:pinterest, :string)
+      add(:instagram, :string)
+
+      add(:qq, :string)
+      add(:weichat, :string)
+      add(:weibo, :string)
+    end
+
+    create(unique_index(:user_socials, [:user_id]))
+  end
+end
