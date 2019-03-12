4e1a72698ce305a3506e6f4dfe1554ec94264fb3
mydearxym <mydearxym@qq.com>
2019-03-12 22:56:10 +0800

    fix(c11n): set camalCase theme edge case

--- lib/mastani_server/accounts/delegates/customization.ex
+++ lib/mastani_server/accounts/delegates/customization.ex
@@ -131,6 +131,8 @@ defmodule MastaniServer.Accounts.Delegate.Customization do
   end
 
   defp downcase_theme("SOLARIZEDDARK"), do: "solarizedDark"
+  defp downcase_theme("solarizedDark"), do: "solarizedDark"
   defp downcase_theme("IRONGREEN"), do: "ironGreen"
+  defp downcase_theme("ironGreen"), do: "ironGreen"
   defp downcase_theme(theme), do: String.downcase(theme)
 end
