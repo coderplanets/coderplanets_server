384ad2b0945875afd48fd256f6f8719751393e5a
mydearxym <mydearxym@qq.com>
2019-03-23 07:46:22 +0800

    refactor(users-table): rm unnecessary social fields

---
+++ priv/repo/migrations/20190322233442_remove_social_fields_in_user.exs
@@ -0,0 +1,19 @@
+defmodule MastaniServer.Repo.Migrations.RemoveSocialFieldsInUser do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      remove(:github)
+      remove(:twitter)
+      remove(:facebook)
+      remove(:zhihu)
+      remove(:dribble)
+      remove(:huaban)
+      remove(:douban)
+      remove(:pinterest)
+      remove(:instagram)
+      remove(:weichat)
+      remove(:weibo)
+    end
+  end
+end
---
+++ priv/repo/migrations/20190322234236_remove_qq_in_user.exs
@@ -0,0 +1,9 @@
+defmodule MastaniServer.Repo.Migrations.RemoveQqInUser do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      remove(:qq)
+    end
+  end
+end
