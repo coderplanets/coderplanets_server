e6d15e34f9e7776118c882f313b5d232a8cd2a03
mydearxym <mydearxym@qq.com>
2019-08-29 22:06:34 +0800

    chore: wip

--- test/helper/sanitizer_test.exs
+++ test/helper/sanitizer_test.exs
@@ -8,9 +8,9 @@ defmodule GroupherServer.Test.Helper.Sanitizer do
   describe "[snaitizer test]" do
     @tag :wip
     test "basic test" do
-      html = "<h1>1</h1><h2>2</h2><h3>3</h3><h4>4</h4><h5>5</h5><h6>6</h6>"
+      html = "hello<h1>1</h1><h2>2</h2><h3>3</h3><h4>4</h4><h5>5</h5><h6>6</h6>world"
       sanitized = Helper.Sanitizer.sanitize(html)
-      assert sanitized = "123456"
+      assert sanitized = "hello123456world"
     end
 
     @tag :wip
