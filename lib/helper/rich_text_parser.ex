b090c865be6f6230bbeabf0d794b87314fa2ddd7
mydearxym <mydearxym@qq.com>
2019-09-01 21:13:55 +0800

    refactor(editor-parser): basic json -> html workflow

--- lib/helper/rich_text_parser.ex
+++ lib/helper/rich_text_parser.ex
@@ -4,8 +4,76 @@ defmodule Helper.RichTextParser do
 
   see https://editorjs.io/
   """
+  @html_class_prefix "cps-viewer"
 
-  def string_to_json(string) do
-    Jason.decode(string)
+  def convert_to_html(string) when is_binary(string) do
+    with {:ok, parsed} = string_to_json(string),
+         true <- valid_editor_data?(parsed) do
+      content =
+        Enum.reduce(parsed["blocks"], "", fn block, acc ->
+          acc <> parse_block(block)
+        end)
+
+      "<div class=\"#{@html_class_prefix}\">#{content}<div>"
+      # |> IO.inspect(label: "hello")
+    end
+  end
+
+  defp parse_block(%{"type" => "header", "data" => data}) do
+    # IO.inspect(data, label: "parse header")
+    text = get_in(data, ["text"])
+    level = get_in(data, ["level"])
+
+    "<h#{level}>#{text}</h#{level}>"
+  end
+
+  defp parse_block(%{"type" => "paragraph", "data" => data}) do
+    # IO.inspect(data, label: "parse paragraph")
+    text = get_in(data, ["text"])
+
+    "<p>#{text}</p>"
+  end
+
+  defp parse_block(%{"type" => "image", "data" => data}) do
+    IO.inspect(data, label: "parse image")
+    url = get_in(data, ["file", "url"])
+
+    "<div class=\"#{@html_class_prefix}-image\"><img src=\"#{url}\"></div>"
+    |> IO.inspect(label: "iamge ret")
+  end
+
+  # defp parse_block(%{"type" => "list", "data" => data}) do
+  #   IO.inspect(data, label: "parse list")
+  # end
+
+  # defp parse_block(%{"type" => "delimiter", "data" => data}) do
+  #   IO.inspect(data, label: "parse delimiter")
+  # end
+
+  # defp parse_block(%{"type" => "linkTool", "data" => data}) do
+  #   IO.inspect(data, label: "parse linkTool")
+  #   data |> get_in(["link"]) |> IO.inspect(label: "linkTool ret")
+  # end
+
+  # defp parse_block(%{"type" => "quote", "data" => data}) do
+  #   IO.inspect(data, label: "parse quote")
+  #   data |> get_in(["text"]) |> IO.inspect(label: "quote ret")
+  # end
+
+  defp parse_block(block) do
+    IO.puts(".")
+    ""
+    # IO.inspect(block, label: "parse unknow")
+  end
+
+  def string_to_json(string), do: Jason.decode(string)
+
+  defp valid_editor_data?(map) when is_map(map) do
+    Map.has_key?(map, "time") and
+      Map.has_key?(map, "version") and
+      Map.has_key?(map, "blocks") and
+      is_list(map["blocks"]) and
+      is_binary(map["version"]) and
+      is_integer(map["time"])
   end
 end
--- test/helper/rich_text_parser_test.exs
+++ test/helper/rich_text_parser_test.exs
@@ -5,9 +5,135 @@ defmodule GroupherServer.Test.Helper.RichTextParserTest do
 
   alias Helper.RichTextParser, as: Parser
 
+  @real_editor_data ~S({
+    "time" : 1567250876713,
+    "blocks" : [
+        {
+            "type" : "header",
+            "data" : {
+                "text" : "Editor.js",
+                "level" : 2
+            }
+        },
+        {
+            "type" : "paragraph",
+            "data" : {
+                "text" : "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text."
+            }
+        },
+        {
+            "type" : "header",
+            "data" : {
+                "text" : "Key features",
+                "level" : 3
+            }
+        },
+        {
+            "type" : "list",
+            "data" : {
+                "style" : "unordered",
+                "items" : [
+                    "It is a block-styled editor",
+                    "It returns clean data output in JSON",
+                    "Designed to be extendable and pluggable with a simple API"
+                ]
+            }
+        },
+        {
+            "type" : "header",
+            "data" : {
+                "text" : "What does it mean «block-styled editor»",
+                "level" : 3
+            }
+        },
+        {
+            "type" : "paragraph",
+            "data" : {
+                "text" : "Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class=\"cdx-marker\">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure\) provided by Plugin and united by Editor's Core."
+            }
+        },
+        {
+            "type" : "paragraph",
+            "data" : {
+                "text" : "There are dozens of <a href=\"https://github.com/editor-js\">ready-to-use Blocks</a> and the <a href=\"https://editorjs.io/creating-a-block-tool\">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games."
+            }
+        },
+        {
+            "type" : "header",
+            "data" : {
+                "text" : "What does it mean clean data output",
+                "level" : 3
+            }
+        },
+        {
+            "type" : "paragraph",
+            "data" : {
+                "text" : "Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below"
+            }
+        },
+        {
+            "type" : "paragraph",
+            "data" : {
+                "text" : "Given data can be used as you want: render with HTML for <code class=\"inline-code\">Web clients</code>, render natively for <code class=\"inline-code\">mobile apps</code>, create markup for <code class=\"inline-code\">Facebook Instant Articles</code> or <code class=\"inline-code\">Google AMP</code>, generate an <code class=\"inline-code\">audio version</code> and so on."
+            }
+        },
+        {
+            "type" : "paragraph",
+            "data" : {
+                "text" : "Clean data is useful to sanitize, validate and process on the backend."
+            }
+        },
+        {
+            "type" : "delimiter",
+            "data" : {}
+        },
+        {
+            "type" : "paragraph",
+            "data" : {
+                "text" : "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏"
+            }
+        },
+        {
+            "type" : "image",
+            "data" : {
+                "file" : {
+                    "url" : "https://codex.so/upload/redactor_images/o_e48549d1855c7fc1807308dd14990126.jpg"
+                },
+                "caption" : "",
+                "withBorder" : true,
+                "stretched" : false,
+                "withBackground" : false
+            }
+        },
+        {
+            "type" : "linkTool",
+            "data" : {
+                "link" : "https://www.github.com",
+                "meta" : {
+                    "url" : "https://www.github.com",
+                    "domain" : "www.github.com",
+                    "title" : "Build software better, together",
+                    "description" : "GitHub is where people build software. More than 40 million people use GitHub to discover, fork, and contribute to over 100 million projects.",
+                    "image" : {
+                        "url" : "https://github.githubassets.com/images/modules/open_graph/github-logo.png"
+                    }
+                }
+            }
+        },
+        {
+            "type" : "quote",
+            "data" : {
+                "text" : "hello world",
+                "caption" : "desc?",
+                "alignment" : "left"
+            }
+        }
+    ],
+    "version" : "2.15.0"
+  })
+
   describe "[basic convert]" do
-    @tag :wip
-    test "string_json should work" do
+    test "basic string_json should work" do
       string = ~S({"time":1566184478687,"blocks":[{}],"version":"2.15.0"})
       {:ok, converted} = Parser.string_to_json(string)
 
@@ -15,7 +141,6 @@ defmodule GroupherServer.Test.Helper.RichTextParserTest do
       assert converted["version"] == "2.15.0"
     end
 
-    @tag :wip
     test "invalid string data should get error" do
       string = ~S({"time":1566184478687,"blocks":[{}],"version":})
       assert {:error, converted} = Parser.string_to_json(string)
@@ -23,113 +148,19 @@ defmodule GroupherServer.Test.Helper.RichTextParserTest do
 
     @tag :wip
     test "real-world editor.js data should work" do
-      string = ~S({
-        "time" : 1567250876713,
-        "blocks" : [
-            {
-                "type" : "header",
-                "data" : {
-                    "text" : "Editor.js",
-                    "level" : 2
-                }
-            },
-            {
-                "type" : "paragraph",
-                "data" : {
-                    "text" : "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text."
-                }
-            },
-            {
-                "type" : "header",
-                "data" : {
-                    "text" : "Key features",
-                    "level" : 3
-                }
-            },
-            {
-                "type" : "list",
-                "data" : {
-                    "style" : "unordered",
-                    "items" : [
-                        "It is a block-styled editor",
-                        "It returns clean data output in JSON",
-                        "Designed to be extendable and pluggable with a simple API"
-                    ]
-                }
-            },
-            {
-                "type" : "header",
-                "data" : {
-                    "text" : "What does it mean «block-styled editor»",
-                    "level" : 3
-                }
-            },
-            {
-                "type" : "paragraph",
-                "data" : {
-                    "text" : "Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class=\"cdx-marker\">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure\) provided by Plugin and united by Editor's Core."
-                }
-            },
-            {
-                "type" : "paragraph",
-                "data" : {
-                    "text" : "There are dozens of <a href=\"https://github.com/editor-js\">ready-to-use Blocks</a> and the <a href=\"https://editorjs.io/creating-a-block-tool\">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games."
-                }
-            },
-            {
-                "type" : "header",
-                "data" : {
-                    "text" : "What does it mean clean data output",
-                    "level" : 3
-                }
-            },
-            {
-                "type" : "paragraph",
-                "data" : {
-                    "text" : "Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below"
-                }
-            },
-            {
-                "type" : "paragraph",
-                "data" : {
-                    "text" : "Given data can be used as you want: render with HTML for <code class=\"inline-code\">Web clients</code>, render natively for <code class=\"inline-code\">mobile apps</code>, create markup for <code class=\"inline-code\">Facebook Instant Articles</code> or <code class=\"inline-code\">Google AMP</code>, generate an <code class=\"inline-code\">audio version</code> and so on."
-                }
-            },
-            {
-                "type" : "paragraph",
-                "data" : {
-                    "text" : "Clean data is useful to sanitize, validate and process on the backend."
-                }
-            },
-            {
-                "type" : "delimiter",
-                "data" : {}
-            },
-            {
-                "type" : "paragraph",
-                "data" : {
-                    "text" : "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏"
-                }
-            },
-            {
-                "type" : "image",
-                "data" : {
-                    "file" : {
-                        "url" : "https://codex.so/upload/redactor_images/o_e48549d1855c7fc1807308dd14990126.jpg"
-                    },
-                    "caption" : "",
-                    "withBorder" : true,
-                    "stretched" : false,
-                    "withBackground" : false
-                }
-            }
-        ],
-        "version" : "2.15.0"
-      })
-      {:ok, converted} = Parser.string_to_json(string)
-      IO.inspect(converted, label: "haha")
+      {:ok, converted} = Parser.string_to_json(@real_editor_data)
 
       assert not Enum.empty?(converted["blocks"])
+      assert converted["blocks"] |> is_list
+      assert converted["version"] |> is_binary
+      assert converted["time"] |> is_integer
+    end
+
+    @tag :wip
+    test "todo" do
+      #   IO.inspect(converted, label: "haha")
+      Parser.convert_to_html(@real_editor_data)
+      #   assert not Enum.empty?(converted["blocks"])
     end
   end
 end
