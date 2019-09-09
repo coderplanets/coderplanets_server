78dd836460331fe57c626c370297410ee58f33af
mydearxym <xieym@chinawyny.com>
2019-09-09 17:42:21 +0800

    chore: add specs

--- lib/helper/guardian.ex
+++ lib/helper/guardian.ex
@@ -7,19 +7,23 @@ defmodule Helper.Guardian do
 
   @token_expireation 24 * 14
 
+  @spec subject_for_token(atom | %{id: any}, any) :: {:ok, binary}
   def subject_for_token(resource, _claims) do
     {:ok, to_string(resource.id)}
   end
 
+  @spec resource_from_claims(nil | keyword | map) :: {:ok, %{id: any}}
   def resource_from_claims(claims) do
     {:ok, %{id: claims["sub"]}}
   end
 
+  @spec jwt_encode(any, map) :: {:error, any} | {:ok, binary, map}
   def jwt_encode(source, args \\ %{}) do
     encode_and_sign(source, args, ttl: {@token_expireation, :hour})
   end
 
   # jwt_decode
+  @spec jwt_decode(binary) :: {:error, any} | {:ok, any, map}
   def jwt_decode(token) do
     resource_from_token(token)
   end
