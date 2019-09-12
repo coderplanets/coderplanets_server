26fd8ede6d98acd61c094643c9b273b89c95aaf8
mydearxym <xieym@chinawyny.com>
2019-09-12 17:34:45 +0800

    chore(editorjs): common markdown parse

--- lib/helper/converter/md_to_editor.ex
+++ lib/helper/converter/md_to_editor.ex
@@ -26,13 +26,12 @@ defmodule Helper.Converter.MdToEditor do
   defp parse_block({type, _opt, content})
        when type in @supported_header do
     content_text =
-      Enum.reduce(content, [], fn content_item, acc ->
+      Enum.reduce(content, "", fn content_item, acc ->
         parsed = parse_inline(type, content_item)
-        acc ++ parsed
+        acc <> parsed
       end)
 
     # IO.inspect(content_text, label: "h-type content_text")
-
     [_, level] = String.split(type, "h")
     level = String.to_integer(level)
 
@@ -46,11 +45,28 @@ defmodule Helper.Converter.MdToEditor do
   end
 
   # parse markdown paragraph to editor's paragraph
+  # parse image
+  defp parse_block({"p", _opt, [{"img", [{"src", src}, _alt], []}]}) do
+    %{
+      type: "image",
+      data: %{
+        file: %{
+          url: src
+        },
+        caption: "",
+        withBorder: false,
+        stretched: false,
+        withBackground: false
+      }
+    }
+  end
+
   defp parse_block({"p", _opt, content}) do
     content_text =
-      Enum.reduce(content, [], fn content_item, acc ->
+      Enum.reduce(content, "", fn content_item, acc ->
         parsed = parse_inline("p", content_item)
-        acc ++ parsed
+
+        acc <> parsed
       end)
 
     %{
@@ -61,6 +77,81 @@ defmodule Helper.Converter.MdToEditor do
     }
   end
 
+  defp parse_block({"ul", [], content}) do
+    case is_checklist_ul?(content) do
+      # normal ul list
+      false ->
+        items =
+          Enum.reduce(content, [], fn content_item, acc ->
+            parsed = parse_inline(content_item)
+            acc ++ [parsed]
+          end)
+
+        %{
+          type: "list",
+          data: %{
+            style: "unordered",
+            items: items
+          }
+        }
+
+      # checklist
+      true ->
+        items =
+          Enum.reduce(content, [], fn content_item, acc ->
+            parsed = parse_inline(content_item)
+
+            acc ++
+              [
+                %{
+                  # 4 表示 [ ] 或 [x] 占用的 4 个 size
+                  "text" => String.slice(parsed, 4, byte_size(parsed)),
+                  "checked" => String.starts_with?(parsed, ["[x] ", "[X] "])
+                }
+              ]
+          end)
+
+        %{
+          type: "checklist",
+          data: %{
+            items: items
+          }
+        }
+    end
+  end
+
+  defp parse_block({"ol", [], content}) do
+    items =
+      Enum.reduce(content, [], fn content_item, acc ->
+        parsed = parse_inline(content_item)
+        acc ++ [parsed]
+      end)
+
+    %{
+      type: "list",
+      data: %{
+        style: "ordered",
+        items: items
+      }
+    }
+  end
+
+  defp parse_block({"blockquote", _opt, content}) do
+    %{
+      type: "quote",
+      data: %{
+        text: parse_inline(content)
+      }
+    }
+  end
+
+  defp parse_block({"hr", _opt, _content}) do
+    %{
+      type: "delimiter",
+      data: {}
+    }
+  end
+
   defp parse_block({_type, _opt, _content}) do
     # IO.inspect(name, label: "parse block")
     # IO.inspect(content, label: "content")
@@ -68,26 +159,68 @@ defmodule Helper.Converter.MdToEditor do
   end
 
   # 字符串直接返回，作为 editor.js 中的 text/data/code 等字段
-  defp parse_inline(content) when is_binary(content) do
-    content
+  defp parse_inline(content) when is_binary(content), do: content
+  defp parse_inline([_type, content]), do: parse_inline(content)
+  defp parse_inline([content]) when is_binary(content), do: parse_inline(content)
+
+  defp parse_inline({"strong", [], content}) do
+    "<b>#{parse_inline(content)}</b>"
   end
 
-  #  TODO:  editor.js 暂时不支持 del 标签，所以直接返回字符串内容即可
-  defp parse_inline({"del", [], [content]}) do
-    content
+  #  NOTE:  editor.js 暂时不支持 del 标签，所以直接返回字符串内容即可
+  # TODO:  del -> editor.s marker
+  defp parse_inline({"del", [], [content]}), do: content
+  # NOTE:  earmark parse italic as em
+  defp parse_inline({"em", [], [content]}), do: inline_res("em", content)
+  defp parse_inline({"em", [], content}), do: inline_res("em", content)
+  defp parse_inline([{"em", [], content}]), do: inline_res("em", content)
+
+  defp parse_inline({"li", [], content}) when is_list(content) do
+    line =
+      Enum.reduce(content, "", fn content_item, acc ->
+        acc <> parse_inline(content_item)
+      end)
+
+    line
   end
 
-  defp parse_inline(_type, content) when is_binary(content) do
-    content
+  defp parse_inline({"li", [], content}), do: parse_inline(content)
+
+  defp parse_inline([{"p", [], content}]) do
+    line =
+      Enum.reduce(content, "", fn content_item, acc ->
+        acc <> parse_inline(content_item)
+      end)
+
+    line
   end
 
-  defp parse_inline(type, {_type, _opt, [content]})
-       when type in @supported_header do
+  defp parse_inline({"code", [{"class", "inline"}], content}), do: inline_res("code", content)
+  defp parse_inline([{"code", [{"class", "inline"}], content}]), do: inline_res("code", content)
+
+  defp parse_inline({"a", [{"href", href}], [content]}) do
+    "<a href=\"#{href}\">#{content}</a>"
+  end
+
+  defp inline_res("em", content), do: "<i>#{parse_inline(content)}</i>"
+
+  defp inline_res("code", content),
+    do: "<code class=\"inline-code\">#{parse_inline(content)}</code>"
+
+  defp parse_inline(_type, content) when is_binary(content), do: content
+
+  defp parse_inline(header, {_type, _opt, [content]})
+       when header in @supported_header do
     parse_inline(content)
   end
 
-  # defp parse_inline({type, _opt, content})
-  #      when type == "h1" or type == "h2" or type == "h3" do
-  #     content
-  # end
+  # when header in @supported_header do
+  defp parse_inline("p", content), do: parse_inline(content)
+
+  # 判断是否为 editorjs 的 checklist
+  defp is_checklist_ul?(content) when is_list(content) do
+    Enum.all?(content, fn {"li", [], li_item} ->
+      li_item |> Enum.at(0) |> String.starts_with?(["[ ] ", "[x] ", "[X] "])
+    end)
+  end
 end
--- test/helper/converter/md_to_editor_test.exs
+++ test/helper/converter/md_to_editor_test.exs
@@ -7,42 +7,226 @@ defmodule GroupherServer.Test.Helper.Converter.MdToEditor do
     @tag :wip
     test "ast parser should work" do
       mdstring = """
+      #  header one
 
-      ## hello
+      header one paragraph
 
-      this is a basic markdown text
+      ## header two
 
-      ### ~~delete me~~
-      this is a basic markdown text
+      header two paragraph
 
-      ### _~~italic me~~_
-      this is a basic markdown text
+      ### header three
+
+      header three paragraph
+
+      --     -> invalid spliter
+
+      valid spliter
+      -
+      valid soliter
+      ---
+      valid soliter
+      ----
+
+      this is a paragraph with **bold-text** and _intalic-text_ and [inline-link](https://cps.fun)
+
+      [block-link](https://cps.fun)
+
+      this is a paragraph with `inline-code-string` in it
+
+      - this is ul list item 1
+      - this is ul list item 2
+      - this is ul list item 3
+
+      1. this is ul list item 1
+      2. this is ul list item 2
+      3. this is ul list item 3
+
+      - [x] this is *checklist* item true
+      - [ ] this is checklist item false
+
+      > this is *quote* texts
+
+      ![image](https://example/example.png)
+      """
+
+      assert Converter.parse(mdstring) == [
+               %{data: %{level: 1, text: "header one"}, type: "header"},
+               %{data: %{text: "header one paragraph"}, type: "paragraph"},
+               %{data: %{level: 2, text: "header two"}, type: "header"},
+               %{data: %{text: "header two paragraph"}, type: "paragraph"},
+               %{data: %{level: 3, text: "header three"}, type: "header"},
+               %{data: %{text: "header three paragraph"}, type: "paragraph"},
+               %{data: %{text: "--     -> invalid spliter"}, type: "paragraph"},
+               %{data: %{level: 2, text: "valid spliter"}, type: "header"},
+               %{data: %{text: "valid soliter"}, type: "paragraph"},
+               %{data: {}, type: "delimiter"},
+               %{data: %{text: "valid soliter"}, type: "paragraph"},
+               %{data: {}, type: "delimiter"},
+               %{
+                 data: %{
+                   text:
+                     "this is a paragraph with <b>bold-text</b> and <i>intalic-text</i> and <a href=\"https://cps.fun\">inline-link</a>"
+                 },
+                 type: "paragraph"
+               },
+               %{
+                 data: %{text: "<a href=\"https://cps.fun\">block-link</a>"},
+                 type: "paragraph"
+               },
+               %{
+                 data: %{
+                   text:
+                     "this is a paragraph with <code class=\"inline-code\">inline-code-string</code> in it"
+                 },
+                 type: "paragraph"
+               },
+               %{
+                 data: %{
+                   items: [
+                     "this is ul list item 1",
+                     "this is ul list item 2",
+                     "this is ul list item 3"
+                   ],
+                   style: "unordered"
+                 },
+                 type: "list"
+               },
+               %{
+                 data: %{
+                   items: [
+                     "this is ul list item 1",
+                     "this is ul list item 2",
+                     "this is ul list item 3"
+                   ],
+                   style: "ordered"
+                 },
+                 type: "list"
+               },
+               %{
+                 data: %{
+                   items: [
+                     %{"checked" => true, "text" => "this is <i>checklist</i> item true"},
+                     %{"checked" => false, "text" => "this is checklist item false"}
+                   ]
+                 },
+                 type: "checklist"
+               },
+               %{data: %{text: "this is <i>quote</i> texts"}, type: "quote"},
+               %{
+                 data: %{
+                   caption: "",
+                   file: %{url: "https://example/example.png"},
+                   stretched: false,
+                   withBackground: false,
+                   withBorder: false
+                 },
+                 type: "image"
+               }
+             ]
+    end
+
+    @tag :wip2
+    test "compose multi inline style in list-item should work" do
+      mdstring = """
+      - this is ul **list** item 1
+      - this is ul _list_ item 2
+      - this is ul _**`list`**_ item 3
+      """
+
+      assert Converter.parse(mdstring) == [
+               %{
+                 data: %{
+                   items: [
+                     "this is ul <b>list</b> item 1",
+                     "this is ul <i>list</i> item 2",
+                     "this is ul <i><b><code class=\"inline-code\">list</code></b></i> item 3"
+                   ],
+                   style: "unordered"
+                 },
+                 type: "list"
+               }
+             ]
+    end
+
+    @tag :wip2
+    test "complex nested markdown rules should not raise error" do
+      mdstring = """
+      this is a paragraph with **_`inline-code-string`*__* in it
+      """
+
+      assert Converter.parse(mdstring) == [
+               %{
+                 data: %{
+                   text:
+                     "this is a paragraph with <i><code class=\"inline-code\">inline-code-string</code></i>__* in it"
+                 },
+                 type: "paragraph"
+               }
+             ]
+
+      mdstring = """
+      this is a paragraph with __**`inline-code-string`*_* in it
+      """
+
+      assert Converter.parse(mdstring) == [
+               %{
+                 data: %{
+                   text:
+                     "this is a paragraph with <i><i><code class=\"inline-code\">inline-code-string</code></i></i>* in it"
+                 },
+                 type: "paragraph"
+               }
+             ]
+    end
+
+    @tag :wip2
+    test "nested inline code should parse right" do
+      mdstring = """
+      this is a paragraph with **`inline-code-string`** in it
       """
 
       res = Converter.parse(mdstring)
 
-      assert res ==
-               [
-                 %{data: %{level: 2, text: "hello"}, type: "header"},
-                 %{
-                   data: %{text: "this is a basic markdown text"},
-                   type: "paragraph"
+      assert res = [
+               %{
+                 data: %{
+                   text:
+                     "this is a paragraph with <b><code class=\"inline-code\">inline-code-string</code></b> in it"
                  },
-                 %{data: %{level: 3, text: "delete me"}, type: "header"},
-                 %{
-                   data: %{text: "this is a basic markdown text"},
-                   type: "paragraph"
+                 type: "paragraph"
+               }
+             ]
+
+      mdstring = """
+      this is a paragraph with **_`inline-code-string`_** in it
+      """
+
+      res = Converter.parse(mdstring)
+
+      assert Converter.parse(mdstring) == [
+               %{
+                 data: %{
+                   text:
+                     "this is a paragraph with <b><i><code class=\"inline-code\">inline-code-string</code></i></b> in it"
                  },
-                 %{data: %{level: 3, text: "italic me"}, type: "header"},
-                 %{
-                   data: %{text: "this is a basic markdown text"},
-                   type: "paragraph"
-                 }
-               ]
+                 type: "paragraph"
+               }
+             ]
 
-      # IO.inspect(res, label: "ast")
+      mdstring = """
+      this is a paragraph with _**`inline-code-string`**_ in it
+      """
 
-      # assert Sanitizer.sanitize(html) == "hello123456<h1>world</h1><h2>world2</h2><h3>world3</h3>"
+      assert Converter.parse(mdstring) == [
+               %{
+                 data: %{
+                   text:
+                     "this is a paragraph with <i><b><code class=\"inline-code\">inline-code-string</code></b></i> in it"
+                 },
+                 type: "paragraph"
+               }
+             ]
     end
 
     @tag :wip2
