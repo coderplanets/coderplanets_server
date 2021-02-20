414b9e7d557818e539f78c7142b1255fbdb6f7e8
mydearxym <mydearxym@qq.com>
2021-02-20 22:14:58 +0800

    feat(utils): add str_occurence method

--- lib/helper/utils.ex
+++ lib/helper/utils.ex
@@ -237,4 +237,13 @@ defmodule Helper.Utils do
   defp update_word_count(word, acc) do
     Map.update(acc, to_string(word), 1, &(&1 + 1))
   end
+
+  # see https://stackoverflow.com/a/49558074/4050784
+  @spec str_occurence(String.t(), String.t()) :: Integer.t()
+  def str_occurence(string, substr) when is_binary(string) and is_binary(substr) do
+    len = string |> String.split(substr) |> length()
+    len - 1
+  end
+
+  def str_occurence(_, _), do: "must be strings"
 end
