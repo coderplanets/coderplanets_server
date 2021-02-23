eb7689596891294ca245c681d6820c53baac0fc9
mydearxym <mydearxym@qq.com>
2021-02-23 23:05:49 +0800

    refactor(validator): make schama validator more flexable

--- lib/helper/validator/schema.ex
+++ lib/helper/validator/schema.ex
@@ -5,7 +5,7 @@ defmodule Helper.Validator.Schema do
   currently support boolean / string / number / enum
   """
 
-  use Helper.Validator.Schema.Matchers, [:string, :number, :list, :boolean]
+  # use Helper.Validator.Schema.Matchers, [:string, :number, :list, :boolean]
 
   @doc """
   cast data by given schema
@@ -49,10 +49,9 @@ defmodule Helper.Validator.Schema do
     end)
   end
 
-  # enum
   defp match(field, nil, enum: _, required: false), do: done(field, nil)
 
-  defp match(field, value, enum: enum, required: false) do
+  defp match(field, value, enum: enum, required: _) do
     match(field, value, enum: enum)
   end
 
@@ -70,7 +69,88 @@ defmodule Helper.Validator.Schema do
     end
   end
 
+  defp match(field, value, [type | options]) do
+    match(field, value, type, options)
+  end
+
+  defp match(field, nil, _type, [{:required, false} | _options]) do
+    done(field, nil)
+  end
+
+  defp match(field, value, type, [{:required, _} | options]) do
+    match(field, value, type, options)
+  end
+
+  # custom validate logic
+  defp match(field, value, :string, [{:min, min} | options])
+       when is_binary(value) and is_integer(min) do
+    case String.length(value) >= min do
+      true ->
+        match(field, value, :string, options)
+
+      false ->
+        error(field, value, :min, min)
+    end
+  end
+
+  defp match(field, value, :number, [{:min, min} | options])
+       when is_integer(value) and is_integer(min) do
+    case value >= min do
+      true ->
+        match(field, value, :number, options)
+
+      false ->
+        error(field, value, :min, min)
+    end
+  end
+
+  # custom validate logic end
+
+  # main type
+  defp match(field, value, :string, []) when is_binary(value) do
+    done(field, value)
+  end
+
+  defp match(field, value, :number, []) when is_integer(value) do
+    done(field, value)
+  end
+
+  defp match(field, value, :list, []) when is_list(value) do
+    done(field, value)
+  end
+
+  defp match(field, value, :boolean, []) when is_boolean(value) do
+    done(field, value)
+  end
+
+  # main type end
+
+  defp match(field, value, _type, [option]) when is_tuple(option) and not is_nil(value) do
+    {k, v} = option
+    error(field, value, option: "#{to_string(k)}: #{to_string(v)}")
+  end
+
+  defp match(field, value, _type, [_option]) when not is_nil(value) do
+    error(field, value, :option)
+  end
+
+  defp match(field, value, type, _) do
+    error(field, value, type)
+  end
+
   defp done(field, value), do: {:ok, %{field: field, value: value}}
+
+  defp error(field, value, :min, min) do
+    {:error, %{field: field |> to_string, value: value, message: "min size: #{min}"}}
+  end
+
+  defp error(field, value, option: option) do
+    {:error, %{field: field |> to_string, value: value, message: "unknow option: #{option}"}}
+  end
+
+  defp error(field, value, :option) do
+    {:error, %{field: field |> to_string, value: value, message: "unknow option"}}
+  end
 
   defp error(field, value, schema) do
     {:error, %{field: field |> to_string, value: value, message: "should be: #{schema}"}}
---
+++ test/helper/validator/schema_test.exs
@@ -0,0 +1,146 @@
+defmodule GroupherServer.Test.Helper.Validator.Schema do
+  @moduledoc false
+
+  use GroupherServerWeb.ConnCase, async: true
+
+  alias Helper.Validator.Schema
+
+  describe "[basic schema]" do
+    @tag :wip
+    test "string with options" do
+      schema = %{"text" => [:string, required: false]}
+      data = %{"no_exsit" => "text"}
+      assert {:ok, _} = Schema.cast(schema, data)
+
+      schema = %{"text" => [:string, required: true]}
+      data = %{"no_exsit" => "text"}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "should be: string", value: nil}]
+
+      schema = %{"text" => [:string, required: true]}
+      data = %{"text" => "text"}
+      assert {:ok, _} = Schema.cast(schema, data)
+
+      schema = %{"text" => [:string, min: 5]}
+      data = %{"text" => "text"}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "min size: 5", value: "text"}]
+
+      schema = %{"text" => [:string, required: false, min: 5]}
+      data = %{"text" => "text"}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "min size: 5", value: "text"}]
+
+      schema = %{"text" => [:string, min: 5]}
+      data = %{"no_exsit" => "text"}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "should be: string", value: nil}]
+
+      schema = %{"text" => [:string, required: true, min: 5]}
+      data = %{"no_exsit" => "text"}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "should be: string", value: nil}]
+
+      schema = %{"text" => [:string, required: true, min: "5"]}
+      data = %{"text" => "text"}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "unknow option: min: 5", value: "text"}]
+      # IO.inspect(Schema.cast(schema, data), label: "schema result")
+    end
+
+    @tag :wip
+    test "number with options" do
+      schema = %{"text" => [:number, required: false]}
+      data = %{"no_exsit" => 1}
+      assert {:ok, _} = Schema.cast(schema, data)
+
+      schema = %{"text" => [:number, required: true]}
+      data = %{"no_exsit" => 1}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "should be: number", value: nil}]
+
+      schema = %{"text" => [:number, required: true]}
+      data = %{"text" => 1}
+      assert {:ok, _} = Schema.cast(schema, data)
+
+      schema = %{"text" => [:number, min: 5]}
+      data = %{"text" => 4}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "min size: 5", value: 4}]
+
+      schema = %{"text" => [:number, required: false, min: 5]}
+      data = %{"text" => 4}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "min size: 5", value: 4}]
+
+      schema = %{"text" => [:number, min: 5]}
+      data = %{"no_exsit" => 4}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "should be: number", value: nil}]
+
+      schema = %{"text" => [:number, required: true, min: 5]}
+      data = %{"no_exsit" => 1}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "should be: number", value: nil}]
+
+      schema = %{"text" => [:number, required: true, min: "5"]}
+      data = %{"text" => 1}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "unknow option: min: 5", value: 1}]
+
+      # IO.inspect(Schema.cast(schema, data), label: "schema result")
+      # hello world
+    end
+
+    @tag :wip
+    test "list with options" do
+      schema = %{"text" => [:list, required: false]}
+      data = %{"no_exsit" => []}
+      assert {:ok, _} = Schema.cast(schema, data)
+
+      schema = %{"text" => [:list, required: true]}
+      data = %{"no_exsit" => []}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "should be: list", value: nil}]
+
+      schema = %{"text" => [:list, required: true]}
+      data = %{"text" => []}
+      assert {:ok, _} = Schema.cast(schema, data)
+    end
+
+    @tag :wip
+    test "boolean with options" do
+      schema = %{"text" => [:boolean, required: false]}
+      data = %{"no_exsit" => false}
+      assert {:ok, _} = Schema.cast(schema, data)
+
+      schema = %{"text" => [:boolean, required: true]}
+      data = %{"no_exsit" => false}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "should be: boolean", value: nil}]
+
+      schema = %{"text" => [:boolean, required: true]}
+      data = %{"text" => false}
+      assert {:ok, _} = Schema.cast(schema, data)
+    end
+
+    @tag :wip
+    test "enum with options" do
+      schema = %{"text" => [enum: [1, 2, 3], required: false]}
+      data = %{"no_exsit" => false}
+      assert {:ok, _} = Schema.cast(schema, data)
+
+      schema = %{"text" => [enum: [1, 2, 3], required: true]}
+      data = %{"no_exsit" => false}
+      {:error, error} = Schema.cast(schema, data)
+      assert error == [%{field: "text", message: "should be: 1 | 2 | 3"}]
+
+      schema = %{"text" => [enum: [1, 2, 3]]}
+      data = %{"text" => 1}
+      assert {:ok, _} = Schema.cast(schema, data)
+
+      # IO.inspect(Schema.cast(schema, data), label: "schema result")
+      # hello world
+    end
+  end
+end
