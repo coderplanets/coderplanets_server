e22113a07ae60332daf3e081c3cdaf9b839f9321
mydearxym <mydearxym@qq.com>
2021-04-16 15:01:55 +0800

    refactor(article-comments): abuse_reports re-org wip

--- test/groupher_server/cms/article_comment_test.exs
+++ test/groupher_server/cms/article_comment_test.exs
@@ -6,7 +6,7 @@ defmodule GroupherServer.Test.CMS.ArticleComment do
   alias Helper.ORM
   alias GroupherServer.CMS
 
-  alias CMS.{AbuseReport, ArticleComment, Post, Job}
+  alias CMS.{ArticleComment, Post, Job}
 
   @delete_hint CMS.ArticleComment.delete_hint()
 
@@ -199,47 +199,6 @@ defmodule GroupherServer.Test.CMS.ArticleComment do
       {:ok, _comment} = CMS.unreport_article_comment(comment.id, user)
       {:ok, comment} = ORM.find(ArticleComment, comment.id)
       assert not comment.is_reported
-    end
-
-    @tag :wip2
-    test "report a comment should have a abuse report record", ~m(user post)a do
-      {:ok, comment} = CMS.create_article_comment(:post, post.id, "commment", user)
-      {:ok, comment} = CMS.report_article_comment(comment.id, user)
-
-      {:ok, all_reports} = ORM.find_all(AbuseReport, %{page: 1, size: 10})
-      report = List.first(all_reports.entries)
-      report_cases = report.report_cases
-
-      assert all_reports.total_count == 1
-      assert report.report_cases_count == 1
-      assert List.first(report_cases).user.login == user.login
-    end
-
-    @tag :wip2
-    test "different user report a comment should have same report with different report cases",
-         ~m(user user2 post)a do
-      {:ok, comment} = CMS.create_article_comment(:post, post.id, "commment", user)
-      {:ok, comment} = CMS.report_article_comment(comment.id, user)
-      {:ok, comment} = CMS.report_article_comment(comment.id, user2)
-
-      {:ok, all_reports} = ORM.find_all(AbuseReport, %{page: 1, size: 10})
-
-      report = List.first(all_reports.entries)
-      report_cases = report.report_cases
-
-      assert all_reports.total_count == 1
-      assert length(report_cases) == 2
-      assert report.report_cases_count == 2
-
-      assert List.first(report_cases).user.login == user.login
-      assert List.last(report_cases).user.login == user2.login
-    end
-
-    @tag :wip2
-    test "same user can not report a comment twice", ~m(user post)a do
-      {:ok, comment} = CMS.create_article_comment(:post, post.id, "commment", user)
-      {:ok, comment} = CMS.report_article_comment(comment.id, user)
-      assert {:error, _} = CMS.report_article_comment(comment.id, user)
     end
   end
 
