798f03423a3091b783567f540fb2a8bfd7b51e7c
mydearxym <mydearxym@qq.com>
2021-05-14 11:00:46 +0800

    refactor: rm collect code from ArticleReaction

--- lib/groupher_server/cms/delegates/article_reaction.ex
+++ lib/groupher_server/cms/delegates/article_reaction.ex
@@ -22,10 +22,6 @@ defmodule GroupherServer.CMS.Delegate.ArticleReaction do
     load_reaction_users(ArticleUpvote, thread, article_id, filter)
   end
 
-  def collected_users(thread, article_id, filter) do
-    load_reaction_users(ArticleCollect, thread, article_id, filter)
-  end
-
   defp load_reaction_users(schema, thread, article_id, %{page: page, size: size} = filter) do
     with {:ok, info} <- match(thread) do
       schema
@@ -33,110 +29,6 @@ defmodule GroupherServer.CMS.Delegate.ArticleReaction do
       |> QueryBuilder.load_inner_users(filter)
       |> ORM.paginater(~m(page size)a)
       |> done()
-    end
-  end
-
-  def collect_article(thread, article_id, %User{id: user_id}) do
-    with {:ok, info} <- match(thread),
-         {:ok, article} <- ORM.find(info.model, article_id, preload: [author: :user]) do
-      Multi.new()
-      |> Multi.run(:inc_author_achieve, fn _, _ ->
-        Accounts.achieve(article.author.user, :inc, :collect)
-      end)
-      |> Multi.run(:inc_article_collects_count, fn _, _ ->
-        update_article_upvotes_count(info, article, :collects_count, :inc)
-      end)
-      |> Multi.run(:update_article_reaction_user_list, fn _, _ ->
-        update_article_reaction_user_list(:collect, article, user_id, :add)
-      end)
-      |> Multi.run(:create_collect, fn _, _ ->
-        thread_upcase = thread |> to_string |> String.upcase()
-        args = Map.put(%{user_id: user_id, thread: thread_upcase}, info.foreign_key, article.id)
-
-        ORM.create(ArticleCollect, args)
-      end)
-      |> Repo.transaction()
-      |> reaction_result()
-    end
-  end
-
-  # 用于在收藏时，用户添加文章到不同的收藏夹中的情况
-  # 如果是同一篇文章，只创建一次，collect_article 不创建记录，只是后续设置不同的收藏夹即可
-  # 如果是第一次收藏，那么才创建文章收藏记录
-  # 避免因为同一篇文章在不同收藏夹内造成的统计和用户成就系统的混乱
-  def collect_article_ifneed(thread, article_id, %User{id: user_id} = user) do
-    with findby_args <- collection_findby_args(thread, article_id, user_id) do
-      already_collected = ORM.find_by(ArticleCollect, findby_args)
-
-      case already_collected do
-        {:ok, article_collect} -> {:ok, article_collect}
-        {:error, _} -> collect_article(thread, article_id, user)
-      end
-    end
-  end
-
-  def undo_collect_article(thread, article_id, %User{id: user_id}) do
-    with {:ok, info} <- match(thread),
-         {:ok, article} <- ORM.find(info.model, article_id, preload: [author: :user]) do
-      Multi.new()
-      |> Multi.run(:dec_author_achieve, fn _, _ ->
-        Accounts.achieve(article.author.user, :dec, :collect)
-      end)
-      |> Multi.run(:inc_article_collects_count, fn _, _ ->
-        update_article_upvotes_count(info, article, :collects_count, :dec)
-      end)
-      |> Multi.run(:update_article_reaction_user_list, fn _, _ ->
-        update_article_reaction_user_list(:collect, article, user_id, :remove)
-      end)
-      |> Multi.run(:undo_collect, fn _, _ ->
-        args = Map.put(%{user_id: user_id}, info.foreign_key, article.id)
-
-        ORM.findby_delete(ArticleCollect, args)
-      end)
-      |> Repo.transaction()
-      |> reaction_result()
-    end
-  end
-
-  def undo_collect_article_ifneed(thread, article_id, %User{id: user_id} = user) do
-    with findby_args <- collection_findby_args(thread, article_id, user_id),
-         {:ok, article_collect} = ORM.find_by(ArticleCollect, findby_args) do
-      case article_collect.collect_folders |> length <= 1 do
-        true -> undo_collect_article(thread, article_id, user)
-        false -> {:ok, article_collect}
-      end
-    end
-  end
-
-  defp collection_findby_args(thread, article_id, user_id) do
-    with {:ok, info} <- match(thread) do
-      thread_upcase = thread |> to_string |> String.upcase()
-      %{thread: thread_upcase, user_id: user_id} |> Map.put(info.foreign_key, article_id)
-    end
-  end
-
-  def set_collect_folder(%ArticleCollect{} = collect, folder) do
-    collect_folders = (collect.collect_folders ++ [folder]) |> Enum.uniq()
-
-    collect
-    |> Ecto.Changeset.change()
-    |> Ecto.Changeset.put_embed(:collect_folders, collect_folders)
-    |> Repo.update()
-  end
-
-  def undo_set_collect_folder(%ArticleCollect{} = collect, folder) do
-    collect_folders = Enum.reject(collect.collect_folders, &(&1.id == folder.id))
-
-    case collect_folders do
-      # means collect already delete
-      [] ->
-        {:ok, :pass}
-
-      _ ->
-        collect
-        |> Ecto.Changeset.change()
-        |> Ecto.Changeset.put_embed(:collect_folders, collect_folders)
-        |> Repo.update()
     end
   end
 
@@ -164,7 +56,7 @@ defmodule GroupherServer.CMS.Delegate.ArticleReaction do
         end
       end)
       |> Repo.transaction()
-      |> reaction_result()
+      |> result()
     end
   end
 
@@ -186,7 +78,7 @@ defmodule GroupherServer.CMS.Delegate.ArticleReaction do
         ORM.find(info.model, article.id)
       end)
       |> Repo.transaction()
-      |> reaction_result()
+      |> result()
     end
   end
 
@@ -248,13 +140,10 @@ defmodule GroupherServer.CMS.Delegate.ArticleReaction do
     ORM.update_meta(article, meta)
   end
 
-  defp reaction_result({:ok, %{create_upvote: result}}), do: result |> done()
-  defp reaction_result({:ok, %{undo_upvote: result}}), do: result |> done()
+  defp result({:ok, %{create_upvote: result}}), do: result |> done()
+  defp result({:ok, %{undo_upvote: result}}), do: result |> done()
 
-  defp reaction_result({:ok, %{create_collect: result}}), do: result |> done()
-  defp reaction_result({:ok, %{undo_collect: result}}), do: result |> done()
-
-  defp reaction_result({:error, _, result, _steps}) do
+  defp result({:error, _, result, _steps}) do
     {:error, result}
   end
 end
