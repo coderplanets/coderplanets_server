37a933b6ef858ae0f3a9574d2a85611a2225f311
mydearxym <mydearxym@qq.com>
2021-05-17 05:18:42 +0800

    refactor(cms-macros): missing doc

--- lib/groupher_server/cms/helper/macros.ex
+++ lib/groupher_server/cms/helper/macros.ex
@@ -13,7 +13,13 @@ defmodule GroupherServer.CMS.Helper.Macros do
   e.g:
   belongs_to(:post, Post, foreign_key: :post_id)
 
-  NOTE: should do migration to DB manually
+  NOTE: should do migration to DB manually:
+  数据库层面的 migration 需要手动添加，参考：
+
+  add(:post_id, references(:cms_posts, on_delete: :delete_all))
+  add(:job_id, references(:cms_jobs, on_delete: :delete_all))
+  add(:repo_id, references(:cms_jobs, on_delete: :delete_all))
+  ...
   """
   defmacro article_belongs_to() do
     @article_threads
