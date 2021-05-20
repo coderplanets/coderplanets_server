9ceb26d0ca82bd0f1dcb460f786d6a694f6ffd27
mydearxym <mydearxym@qq.com>
2021-05-20 14:59:07 +0800

    refactor(community): more test

--- test/groupher_server/cms/community/community_meta_test.exs
+++ test/groupher_server/cms/community/community_meta_test.exs
@@ -58,5 +58,76 @@ defmodule GroupherServer.Test.CMS.Community.CommunityMeta do
       assert community2.meta.articles_count == 1
       assert community2.meta.posts_count == 1
     end
+
+    @tag :wip2
+    test "create a job should inc jobs_count in meta",
+         ~m(user community community2 community3)a do
+      job_attrs = mock_attrs(:job)
+      job_attrs2 = mock_attrs(:job)
+
+      {:ok, _job} = CMS.create_article(community, :job, job_attrs, user)
+      {:ok, _job} = CMS.create_article(community, :job, job_attrs2, user)
+
+      {:ok, _job} = CMS.create_article(community2, :job, job_attrs, user)
+      {:ok, _job} = CMS.create_article(community3, :job, job_attrs, user)
+
+      {:ok, community} = ORM.find(Community, community.id)
+      assert community.meta.articles_count == 2
+      assert community.meta.jobs_count == 2
+
+      {:ok, community2} = ORM.find(Community, community2.id)
+      assert community2.meta.articles_count == 1
+      assert community2.meta.jobs_count == 1
+    end
+
+    @tag :wip2
+    test "create a repo should inc repos_count in meta",
+         ~m(user community community2 community3)a do
+      repo_attrs = mock_attrs(:repo)
+      repo_attrs2 = mock_attrs(:repo)
+
+      {:ok, _repo} = CMS.create_article(community, :repo, repo_attrs, user)
+      {:ok, _repo} = CMS.create_article(community, :repo, repo_attrs2, user)
+
+      {:ok, _repo} = CMS.create_article(community2, :repo, repo_attrs, user)
+      {:ok, _repo} = CMS.create_article(community3, :repo, repo_attrs, user)
+
+      {:ok, community} = ORM.find(Community, community.id)
+      assert community.meta.articles_count == 2
+      assert community.meta.repos_count == 2
+
+      {:ok, community2} = ORM.find(Community, community2.id)
+      assert community2.meta.articles_count == 1
+      assert community2.meta.repos_count == 1
+    end
+
+    @tag :wip2
+    test "create a multi article should inc repos_count in meta",
+         ~m(user community community2)a do
+      post_attrs = mock_attrs(:post)
+      post_attrs2 = mock_attrs(:post)
+
+      job_attrs = mock_attrs(:job)
+      repo_attrs = mock_attrs(:repo)
+
+      {:ok, _} = CMS.create_article(community, :post, post_attrs, user)
+      {:ok, _} = CMS.create_article(community, :post, post_attrs2, user)
+      {:ok, _} = CMS.create_article(community, :job, job_attrs, user)
+
+      {:ok, _} = CMS.create_article(community2, :job, job_attrs, user)
+      {:ok, _} = CMS.create_article(community2, :repo, repo_attrs, user)
+
+      {:ok, community} = ORM.find(Community, community.id)
+      assert community.meta.articles_count == 3
+      assert community.meta.posts_count == 2
+      assert community.meta.jobs_count == 1
+      assert community.meta.repos_count == 0
+
+      {:ok, community2} = ORM.find(Community, community2.id)
+      assert community2.meta.articles_count == 2
+      assert community2.meta.posts_count == 0
+      assert community2.meta.jobs_count == 1
+      assert community2.meta.repos_count == 1
+    end
   end
 end
