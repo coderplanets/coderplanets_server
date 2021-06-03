8d82bf5c3c01e092c2beb4e895c3b5e09fe711e9
xieyimian <mydearxym@qq.com>
2021-06-03 14:16:47 +0800

    refactor(cert): use @article_threads to auto generate rules (#384)

--- lib/groupher_server_web/middleware/passport.ex
+++ lib/groupher_server_web/middleware/passport.ex
@@ -9,7 +9,7 @@
 defmodule GroupherServerWeb.Middleware.Passport do
   @moduledoc """
   c? -> community / communities
-  t? -> thread, could be post / job / tut ...
+  t? -> article thread, could be post / job / tut ...
   """
   @behaviour Absinthe.Middleware
 
