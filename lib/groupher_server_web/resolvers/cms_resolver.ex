82a162bcc7a934659c485735d7e765e12066f7c4
mydearxym <mydearxym@qq.com>
2021-06-04 10:42:39 +0800

    refactor(dir): fix build error

--- lib/groupher_server_web/resolvers/cms_resolver.ex
+++ lib/groupher_server_web/resolvers/cms_resolver.ex
@@ -7,7 +7,7 @@ defmodule GroupherServerWeb.Resolvers.CMS do
   alias GroupherServer.{Accounts, CMS}
 
   alias Accounts.User
-  alias CMS.Model.{Community, Category, Thread}
+  alias CMS.Model.{Community, Category, Thread, CommunityWiki, CommunityCheatsheet}
 
   alias Helper.ORM
 
@@ -380,7 +380,7 @@ defmodule GroupherServerWeb.Resolvers.CMS do
   end
 
   def add_wiki_contributor(_root, ~m(id contributor)a, %{context: %{cur_user: _user}}) do
-    CMS.add_contributor(%CMS.CommunityWiki{id: id}, contributor)
+    CMS.add_contributor(%CommunityWiki{id: id}, contributor)
   end
 
   def sync_cheatsheet(_root, ~m(community_id readme last_sync)a, %{context: %{cur_user: _user}}) do
@@ -388,7 +388,7 @@ defmodule GroupherServerWeb.Resolvers.CMS do
   end
 
   def add_cheatsheet_contributor(_root, ~m(id contributor)a, %{context: %{cur_user: _user}}) do
-    CMS.add_contributor(%CMS.CommunityCheatsheet{id: id}, contributor)
+    CMS.add_contributor(%CommunityCheatsheet{id: id}, contributor)
   end
 
   def search_communities(_root, %{title: title}, _info) do
