e5940aef34a80641a1af2ed4ed77b723b0b21a7f
mydearxym <mydearxym@qq.com>
2021-11-03 23:32:05 +0800

    refactor(workflow): clean up

--- test/groupher_server_web/mutation/cms/articles/works_test.exs
+++ test/groupher_server_web/mutation/cms/articles/works_test.exs
@@ -88,7 +88,6 @@ defmodule GroupherServer.Test.Mutation.Articles.Works do
       }
     }
     """
-    @tag :wip
     test "create works with valid attrs and make sure author exsit", ~m(community)a do
       {:ok, user} = db_insert(:user)
       user_conn = simu_conn(:user, user)
