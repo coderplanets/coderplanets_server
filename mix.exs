1091acd229b415868f3bdf830572214d63af9244
mydearxym <mydearxym@qq.com>
2021-11-22 21:28:33 +0800

    chore(deploy): force update for build debug

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule GroupherServer.Mixfile do
   def project do
     [
       app: :groupher_server,
-      version: "2.0",
+      version: "2.0.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
